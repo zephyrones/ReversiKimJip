f41bab36928b7e5c293555b35c76ef8c370b672a
Kim Nieuwets <k.l.nieuwets@students.uu.nl>
2022-12-15 12:38:21 +0100

    we hebben nu spelbord en steentjes

---
+++ Reversiv2/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace Reversiv2_Namespace
+{
+    class Stenen 
+    {
+        public void tekenSteen(object o, PaintEventArgs pea)
+        {       
+            Graphics gr = pea.Graphics;
+            gr.FillEllipse(Brushes.Indigo, 20, 20, 50, 50); 
+        } 
+    }
+}
--- Reversiv2/Reversiv2.cs
+++ Reversiv2/Reversiv2.cs
@@ -3,18 +3,29 @@ using System.Drawing;
 using System.Windows.Forms;
 
 Application.Run(new Speelbord());
+
+
+class Stenen
+{
+    public void tekenSteen(object o, PaintEventArgs pea)
+    {
+        Graphics gr = pea.Graphics;
+        gr.FillEllipse(Brushes.Indigo, 20, 20, 50, 50);
+    }
+}
 class Speelbord : Form
 {
-    private int Midden_x = 200;
-    private int Midden_y = 200;
-    private int n = 6;
+    public int Midden_x = 200;
+    public int Midden_y = 200;
+    public int n = 6;
     public Speelbord()
     {
         this.Size = new Size(500,500);
         this.BackColor = Color.White;
         this.Paint += this.tekenSpeelbord;
+        this.Paint += this.tekenSteenB;
+        this.Paint += this.tekenSteenR;
     }
-
 
 private void tekenSpeelbord(object o, PaintEventArgs pea)
     {  
@@ -28,6 +39,18 @@ private void tekenSpeelbord(object o, PaintEventArgs pea)
                 gr.DrawRectangle(Pens.Black, Begin_x + (i * 50), Begin_y + (j * 50), 50, 50);
             }
         }
+    }
 
+    public void tekenSteenB(object o, PaintEventArgs pea)
+    {
+        Graphics gr = pea.Graphics;
+        gr.FillEllipse(Brushes.Blue, 20, 20, 50, 50);
+    }
+
+    public void tekenSteenR(object o, PaintEventArgs pea)
+    {
+        Graphics gr = pea.Graphics;
+        gr.FillEllipse(Brushes.Red, 40, 20, 50, 50);
     }
 }
+
