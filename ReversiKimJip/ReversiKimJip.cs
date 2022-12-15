a885593c35d707510018b919a062e28cebb7ea3e
Kim Nieuwets <k.l.nieuwets@students.uu.nl>
2022-12-15 11:57:04 +0100

    Creating Sub class (or an attempt)

--- ReversiKimJip/ReversiKimJip.cs
+++ ReversiKimJip/ReversiKimJip.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -9,37 +10,42 @@ scherm.ClientSize = new Size(1600, 800);
 // Voor de blauwe steen: Color.Indigo
 // Voor de rode steen: Color.Maroon
 
-// een Label kan ook gebruikt worden om een Bitmap te laten zien
 Label afbeelding = new Label();
 scherm.Controls.Add(afbeelding);
 afbeelding.Location = new Point(600, 200);
 afbeelding.Size = new Size(400, 400);
 afbeelding.BackColor = Color.White;
 
+Button knop = new Button();
+scherm.Controls.Add(knop);
+knop.Location = new Point(20, 20);
+knop.Size = new Size(20, 20);
+knop.Text = "6 x 6";
+
 Bitmap plaatje = new Bitmap(300, 300);
 afbeelding.Image = plaatje;
 
-Graphics gr = Graphics.FromImage(plaatje);
 
-int Midden_x = 800;
-int Midden_y = 400;
-int n = 6;
-
-int Begin_x = Midden_x - ((n / 2) * 50);
-int Begin_y = Midden_y - ((n / 2) * 50);
-
-// for (int i = 0; i < n; i++)
-
-gr.DrawRectangle(Pens.Black, Begin_x, Begin_y, 50, 50); // standaard grootte voor 1 vierkantje
+// subclass of Form to create the checkerboard field
+class Checkerboard : Form
+{
+    private int Midden_x = 800;
+    private int Midden_y = 400;
+    private int n = 6;
+    Graphics gr = Graphics.FromImage(plaatje);
 
 
-
-
-
-
-
-
-
+    void DrawVierkant(object o, PaintEventArgs pea)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            int Begin_x = Midden_x - ((n / 2) * 50);
+            int Begin_y = Midden_y - ((n / 2) * 50);
+            gr.DrawRectangle(Pens.Black, Begin_x + (i * 50), Begin_y, 50, 50);
+        }
+        afbeelding.Invalidate();
+    }
+}
 
 
 // HET EINDE
